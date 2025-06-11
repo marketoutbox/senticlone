ff7070808c1103a280535727b274d82a2f924686
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-06-11 16:50:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import SentimentDashboard from "../sentiment-dashboard"
+import WinRateDisplay from "@/components/win-rate-display"
 
 export default function Home() {
-  return <SentimentDashboard />
+  return (
+    <>
+      <WinRateDisplay />
+      <SentimentDashboard />
+    </>
+  )
 }
-
